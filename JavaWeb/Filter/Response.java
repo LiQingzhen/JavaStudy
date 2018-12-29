63932b95574ba64797222ac370b43f298e306b03
LiQingzhen <liqingzhen01@126.com>
2018-12-29 17:12:27 +0800

    commit Response.java

---
+++ JavaWeb/Filter/Response.java
@@ -0,0 +1,20 @@
+package personal.lab01;
+
+public class Response {
+	private String string;
+
+	/**
+	 * @return the string
+	 */
+	public String getString() {
+		return string;
+	}
+
+	/**
+	 * @param string the string to set
+	 */
+	public void setString(String string) {
+		this.string = string;
+	}
+	
+}

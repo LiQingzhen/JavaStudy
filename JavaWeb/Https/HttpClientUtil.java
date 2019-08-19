309dc3db75fa3d44aea5d03e575eb612d0a63188
Qingzhen Li <liqingzhen01@126.com>
2019-08-19 16:49:10 +0800

    https request demo

---
+++ JavaWeb/Https/HttpClientUtil.java
@@ -0,0 +1,39 @@
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientUtil {
+	
+	public String doPost(String url, String jsonData, String charset) {
+		
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		String result = null;
+		try {
+			httpClient = new SSLClient();
+			httpPost = new HttpPost(url);
+			httpPost.addHeader("Content-Type", "application/json;charset=utf-8");
+			
+			StringEntity entity = new StringEntity(jsonData, "utf-8");
+			entity.setContentEncoding("UTF-8");
+	        entity.setContentType("application/json");
+	        httpPost.setEntity(entity);
+			
+			
+			HttpResponse response = httpClient.execute(httpPost);
+			if (response != null) {
+				HttpEntity resEntity = response.getEntity();
+				if (resEntity != null) {
+					result = EntityUtils.toString(resEntity, charset);
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return result;
+	}
+}
